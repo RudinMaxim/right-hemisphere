92d88c7eb790178be21d35e6efc0f826be04aec2
Maxim Rudin <>
2024-05-18 17:04:34 +0500

    fx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { HomePage, PostPage } from './pages';
-import { NotFoundPage } from './pages/NotFoundPage';
 
 const AppRoutes: React.FC = () => {
   return (
@@ -9,7 +8,7 @@ const AppRoutes: React.FC = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/message/:postId" element={<PostPage />} />
-        <Route path="*" element={<NotFoundPage />} />
+        {/* <Route path="*" element={<NotFoundPage />} /> */}
       </Routes>
     </Router>
   );
