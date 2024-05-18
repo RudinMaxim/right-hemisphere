8fdb4662c66e01f0cd55807a8b872d3fce136bb2
Maxim Rudin <>
2024-05-18 17:55:18 +0500

    en router

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import { Layout } from 'antd';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { RouterProvider } from 'react-router-dom';
 import './index.css';
-import AppRoutes from './routes.tsx';
+import { router } from './routes.tsx';
 import store from './store/store.ts';
 
 const { Content, Footer } = Layout;
@@ -13,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Provider store={store}>
             <Layout className="ant-layout">
                 <Content className="ant-layout-content">
-                    <AppRoutes />
+                    <RouterProvider router={router} />
                 </Content>
                 <Footer style={{ textAlign: 'center' }}>
                     2024 © Рудин Максим, Тестовое задание: для Правого
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import {
+  createBrowserRouter
+} from 'react-router-dom';
 import { HomePage, PostPage } from './pages';
+import { NotFoundPage } from './pages/NotFoundPage';
 
-const AppRoutes: React.FC = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/message/:postId" element={<PostPage />} />
-        {/* <Route path="*" element={<NotFoundPage />} /> */}
-      </Routes>
-    </Router>
-  );
-};
-
-export default AppRoutes;
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <HomePage />,
+    },
+    {
+        path: '/message/:postId',
+        element: <PostPage />,
+    },
+    {
+        path: '*',
+        element: <NotFoundPage />,
+    },
+]);
